9b4398f1af4dc3f178dfacfe223c4ae4aeab1349
ridwan101 <78007081+ridwan101@users.noreply.github.com>
2022-04-05 21:45:09 +0600

    Add files via upload

---
+++ Bank Management/Bank Management/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bank_Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
